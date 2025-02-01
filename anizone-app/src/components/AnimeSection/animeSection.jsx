713d8e8b220aec0f8711d2c5c646a14b290a6f51
Haentis <iskanderrr06@mail.ru>
2025-02-01 18:20:45 +0500

    add AnimeSection component

---
+++ anizone-app/src/components/AnimeSection/animeSection.jsx
@@ -0,0 +1,16 @@
+import './animeSection.css';
+
+
+function AnimeSection(){
+    return(
+        <>
+            <div className="main">
+            
+            </div>
+        </>
+    )
+}
+
+
+
+export default AnimeSection;
--- anizone-app/src/pages/Home.jsx
+++ anizone-app/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Banner from "../components/Banner/Banner";
 import Header from "../components/header/header";
 import Nav from "../components/nav/nav";
-
+import AnimeSection from "../components/AnimeSection/animeSection";
 
 function Home(){
     return(
         <>
             <Header/>
             <Banner/>
+            <AnimeSection/>
             <Nav/>
         </>
     )
