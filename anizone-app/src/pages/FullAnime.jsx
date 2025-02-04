f5f7b159b6a42e888c5b7a7dcb051f464f02ff6e
Haentis <iskanderrr06@mail.ru>
2025-02-04 23:06:17 +0500

    fix

--- anizone-app/src/pages/FullAnime.jsx
+++ anizone-app/src/pages/FullAnime.jsx
@@ -1,6 +1,6 @@
 import Nav from "../components/nav/nav";
 import "../components/FullAnime/FullAnime.css";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 
@@ -42,15 +42,40 @@ function FullAnime(){
            return <div className="loader"></div>
         }
 
+        const addToLibrary = () => {
+            // Получить текущий массив ID из LocalStorage или создать пустой
+            const animeIds = JSON.parse(localStorage.getItem('id_anime')) || [];
+        
+            // Проверить, если ID уже существует, чтобы не добавлять дубли
+            if (!animeIds.includes(id)) {
+                animeIds.push(id); // Добавить новый ID
+            }
+        
+            // Сохранить обновлённый массив обратно в LocalStorage
+            localStorage.setItem('id_anime', JSON.stringify(animeIds));
+        };
+        
+
     return(
         <>
                 <section>
                     <div className="left-info-anime">
                         {animeInfo && <img src={`https://shikimori.one${animeInfo.image.original}`} className='image-info' />}
+                        <Link to={'/watch'} className="watch-link">Смотреть</Link>
+                        <button className="add-button" onClick={addToLibrary}>В Библеотеку</button>
                     </div>
                     <div className="info-anime">
                         {animeInfo && <h1  className="header-h1">{animeInfo.russian}</h1>}
                         {animeInfo && <p className="desc-anime">{animeInfo.description}</p>}
+                        <div className="genres-block">
+                            {animeInfo.genres ? (
+                                (animeInfo.genres.map((genreses, index) => (
+                                            <div key={index} className="genres"><p>{genreses.russian}</p></div>
+                                )))
+                            ):(
+                                <p>Не найдено жанра</p>
+                            )}
+                        </div>
                     </div>
                 </section>
             <Nav/>
--- anizone-app/src/pages/Library.jsx
+++ anizone-app/src/pages/Library.jsx
@@ -1,16 +1,22 @@
 import Header from "../components/header/header";
 import Nav from "../components/nav/nav";
-// import nav from "../components/nav/nav";
+import '../components/Library/Library.css';
+import { useEffect, useState } from "react";
 
 
-function Library(){
-    return(
-        <>
-            <Header/>
-            <Nav/>
-        </>
-    )
-};
+    function Library(){
+        return(
+    
+            <>
+                <section>
+                  
+                    <p style={{color:'red', fontSize:'40px'}}>В разработке</p>
+                </section>
+                <Nav/>
+            </>
+        )
+    }
+    
 
 export default Library;
 
